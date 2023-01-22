3ab245a6c7af12228ce2c5096136bf1b64093e2c
Wendelin Gerein <info@webdelin.de>
2023-01-22 23:36:57 +0100

    add prettier

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,16 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
+      <v-icon icon="mdi-circle-slice-6" @click="clicFunc()"/>
+      Essentials Preset {{ testConst }}
     </v-app-bar-title>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-  //
+import { ref } from 'vue';
+const testConst = ref(false)
+function clicFunc(){
+  testConst.value = !testConst.value
+}
 </script>
