c1de23c5c9f63f4a75d61fe55b6812b76ec0d3b6
Wendelin Gerein <info@webdelin.de>
2023-02-19 13:09:59 +0100

    refactor lint

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,16 +1,29 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" @click="clicFunc()"/>
-      Essentials Preset {{ testConst }}
-    </v-app-bar-title>
-  </v-app-bar>
+    <v-toolbar>
+      <v-app-bar-nav-icon  @click="clickFunc()"/>
+
+      <v-toolbar-title>Title - {{testConst}}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
 import { ref } from 'vue';
 const testConst = ref(false)
-function clicFunc(){
+function clickFunc(): void{
   testConst.value = !testConst.value
 }
 </script>
--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,12 +1,24 @@
 <template>
-  <v-app>
+  <v-app :theme="theme">
+    <v-main>
     <default-bar />
 
     <default-view />
+    <v-btn
+        :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+        @click="onClick"
+      >Toggle Theme</v-btn>
+    </v-main>
   </v-app>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
 import DefaultBar from "./AppBar.vue";
 import DefaultView from "./View.vue";
+import { ref } from 'vue';
+const theme = ref('light');
+
+  function onClick (): void {
+    theme.value = theme.value === 'light' ? 'dark' : 'light'
+  }
 </script>
